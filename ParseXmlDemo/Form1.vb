827a14a3e92269d139a68cac4673c43d8f73708f
Shreyash Banawala <shreyash.banawala@ics-global.in>
2022-05-10 18:50:49 +0530

    New Project

---
+++ ParseXmlDemo/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Xml
+Imports System.Xml.Serialization
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ListBox1().Items.Clear()
+        OpenFileDialog1.Title = "Please select an XML file"
+        OpenFileDialog1.InitialDirectory = "C:\"
+        OpenFileDialog1.Filter = "XML Files|*.xml"
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+
+            Dim students As New StudentCollection()
+            Dim serializer As New XmlSerializer(GetType(StudentCollection))
+            Dim reader As New System.IO.StreamReader(OpenFileDialog1.FileName)
+            students = CType(serializer.Deserialize(reader), StudentCollection)
+
+            For Each std As Student In students.Student
+                Dim Id As String = "Id : " & std.Id.ToString()
+                ListBox1.Items.Add(Id)
+                Id = "Name : " & std.Name.ToString()
+                ListBox1.Items.Add(Id)
+                Id = "Percentage : " & std.Percentage.ToString()
+                ListBox1.Items.Add(Id)
+            Next
+
+        End If
+    End Sub
+End Class
---
+++ ParseXmlDemo/Student.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Xml.Serialization
+<Serializable()>
+Public Class Student
+    <XmlElement("Id")>
+    Public Property Id() As Integer
+    <XmlElement("Name")>
+    Public Property Name() As String
+    <XmlElement("Percentage")>
+    Public Property Percentage() As String
+End Class
+<Serializable(), System.Xml.Serialization.XmlRoot("StudentCollection")>
+Public Class StudentCollection
+    <XmlArray("Students"), XmlArrayItem("Student", GetType(Student))>
+    Public Property Student() As Student()
+End Class
